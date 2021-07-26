9f0fa92a9285d212d55507a4414a605afebe5a1c
Juan <76711419+JuqnDev@users.noreply.github.com>
2021-07-25 22:11:39 -0300

    Create Arrow.php

---
+++ src/juqn/special/entities/projectile/Arrow.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace juqn\special\entities\projectile;
+
+use pocketmine\entity\projectile\Arrow as ArrowDefault;
+
+/**
+ * Class Arrow
+ * @package juqn\special\entities\projectile
+ */
+class Arrow extends ArrowDefault
+{
+}
